42d8a662f9282dbe25acb7d88c75582ef862d90f
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-09 09:02:24 -0500

    Updated home page and removed access but need to add functionality.

--- GHS/GHS/Pages/Index.cshtml.cs
+++ GHS/GHS/Pages/Index.cshtml.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GHS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GHS.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly GHS.Models.GHSContext _context;
 
+        public IndexModel(GHS.Models.GHSContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ProductModel> ProductModel { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            ProductModel = await _context.ProductModel.ToListAsync();
         }
     }
 }
